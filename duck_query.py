8e880dc779997a903a4103dfc4ca4d363cde20b4
RehanShaikh-ai <rehansk.3107@gmail.com>
2026-01-07 20:04:04 +0530

    add binary status column

--- duck_query.py
+++ duck_query.py
@@ -6,11 +6,20 @@ con = duckdb.connect("nyc_311.duckdb")
 con.execute("""
 CREATE OR REPLACE VIEW v_raw \
             AS
-            SELECT *
+            SELECT *,
+            CASE 
+                WHEN lower(status) IN(
+            'open', 
+            'in progress', 
+            'pending', 
+            'started') THEN 'Open'
+            ELSE 'Unspecified'
+            END AS normalized_status
             FROM 'NYC311.parquet' 
             """)
 
 con.execute("""
-SELECT MAX(created_date) FROM v_raw
-            """).fetchone()
-
+            SELECT normalized_status, status 
+            FROM v_raw 
+            WHERE status = 'Pending' 
+            LIMIT 10 """).fetch_df()
